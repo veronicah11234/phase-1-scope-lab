37950bb6e6b2fa46ada6d6be8db10ee4c9d18c97
miriam <miriamwangui525@gmail.com>
2023-12-14 21:40:49 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+  window.bestCustomer = bestCustomer; 
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer; 
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new least favorite';
+}
